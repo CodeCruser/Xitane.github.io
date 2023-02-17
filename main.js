b2efe886a2c36574f9715c716f64d2dd19cce5a4
El ouardichi Fahd <53102299+Fahd-Elouardichi@users.noreply.github.com>
2023-02-17 15:37:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -28,3 +28,16 @@ const shuffle = array => {
 
     return clonedArray
 }
+const pickRandom = (array, items) => {
+    const clonedArray = [...array]
+    const randomPicks = []
+
+    for (let index = 0; index < items; index++) {
+        const randomIndex = Math.floor(Math.random() * clonedArray.length)
+        
+        randomPicks.push(clonedArray[randomIndex])
+        clonedArray.splice(randomIndex, 1)
+    }
+
+    return randomPicks
+}
